e5abdfd0f34c3c4560f72f2b2a9615907852ed8e
Roma <gereg@gmail.com>
2025-09-24 17:10:55 +0300

    feat: add basic types for utils (number/string)

--- src/demo.ts
+++ src/demo.ts
@@ -1,9 +1,4 @@
 import { add, capitalize } from './index.js';
 
-// ������� ˲������ ��������: ������� �����
-
-console.log('sum(any):', add(2, 3));
-
-// ������� ˲������ ��������: ������������� �����
-
-console.log('capitalize(any):', capitalize('hello'));
+console.log('sum(typed):', add(2, 3));
+console.log('capitalize(typed):', capitalize('hello'));
